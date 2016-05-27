9d8362b6d3aaa6f13662b9e2d9ca33ac3780e323
Abel Salgado Romero <abelromero@gmail.com>
2016-05-27 10:44:41 +0200

    Updated BasicConnection. Output shows what are folders and what documents

--- src/test/groovy/BasicConnection.groovy
+++ src/test/groovy/BasicConnection.groovy
@@ -2,6 +2,7 @@ import com.box.sdk.BoxAPIConnection
 import com.box.sdk.BoxFolder
 import com.box.sdk.BoxItem
 import com.box.sdk.BoxUser
+
 /**
  * Created by ABEL.SALGADOROMERO on 26/02/2016.
  */
@@ -13,16 +14,18 @@ System.setProperty("http.proxyPort", config.proxy.port)
 System.setProperty("https.proxyHost", config.proxy.host)
 System.setProperty("https.proxyPort", config.proxy.port)
 
-
-// Obtain token from
-// https://box-token-generator.herokuapp.com/
+// Easy way tp obtain token: https://box-token-generator.herokuapp.com/
 
 BoxAPIConnection api = new BoxAPIConnection(config.box.token);
 
 println "=== Root elements"
 BoxFolder rootFolder = BoxFolder.getRootFolder(api)
 for (BoxItem.Info item : rootFolder) {
-    println "$item.ID $item.name"
+    if (item.sharedLink) {
+        println item.sharedLink.URL
+    }
+    // Prints (F) for folders and (D) for documents
+    println "${(item instanceof BoxFolder.Info) ? '(F)' : '(D)'} $item.ID $item.name"
 }
 
 println "=== User info"
@@ -30,3 +33,4 @@ BoxUser.Info userInfo = BoxUser.getCurrentUser(api).getInfo()
 System.out.format("Welcome, %s <%s>!\n\n", userInfo.getName(), userInfo.getLogin())
 
 println "DONE!"
+
