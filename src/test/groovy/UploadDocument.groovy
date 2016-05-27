b0b99d9f0525618bab1a716d0263872b61602911
Abel Salgado Romero <abelromero@gmail.com>
2016-05-27 11:00:19 +0200

    Updated UploadDocument demo

--- src/test/groovy/UploadDocument.groovy
+++ src/test/groovy/UploadDocument.groovy
@@ -8,8 +8,7 @@ import com.box.sdk.ProgressListener
 /**
  * Created by ABEL.SALGADOROMERO on 26/02/2016.
  */
-
-def config = new ConfigSlurper().parse(new File('../../../src/main/resources/config.groovy').toURI().toURL())
+def config = new ConfigSlurper().parse(getFile('config.groovy').toURL())
 
 System.setProperty("http.proxyHost", config.proxy.host)
 System.setProperty("http.proxyPort", config.proxy.port)
@@ -29,34 +28,36 @@ println "=== Test ${this.class.name}"
 BoxFolder rootFolder = BoxFolder.getRootFolder(api)
 
 // Get target folder by Id
-BoxFolder folder = new BoxFolder(api, '6831337682')
+BoxFolder folder = new BoxFolder(api, config.box.folder)
 println folder.info.name
 
-// def is = getResource('Test Document 1.docx')
-// def is = getResource('sample_10.txt')
-// def is = getResource('sample_50.txt')
-def is = getResource('sample_700.txt')
+def filename = 'docs/Test Document 1.docx'
 
 // Closest thing to a DSL ;)
 BoxFile.Info file = folder.uploadFile([
-        content : is,
+        content : getResource(filename),
         name    : "My_test_file-${System.currentTimeMillis()}.docx",
-        created : new Date() - 10,
+        created : new Date() - 10, /* ContentCreatedAt can be manipulated */
+        size    : getFile(filename).size(),  /* Size must be set manually */
         listener: { numBytes, totalBytes ->
             println "[Uploading] NumKBytes: ${numBytes / 1024} KB - TotalBytes: $totalBytes"
-        } as ProgressListener
+        } as ProgressListener,
 ] as FileUploadParams)
 
-println "$file.ID $file.name"
-println "$file.createdAt - $file.contentCreatedAt"
+println "ID: $file.ID, filenam: $file.name"
+println "CreatedAt: $file.createdAt, ContentCreatedAt: $file.contentCreatedAt"
 
-// Not working yet
-// Metadata metadata = new Metadata()
-// metadata.add("/my_description","some text")
-//metadata.add("my_description","some text2")
+/**
+ * Utils
+ */
+InputStream getResource(String name) {
+    this.class.classLoader.getResourceAsStream(name)
+}
 
-println "DONE!"
+File getFile(String name) {
+    new File(this.class.classLoader.getResource(name).toURI())
+}
 
-def getResource(String name) {
-    this.class.classLoader.getResourceAsStream("docs/$name")
+addShutdownHook {
+    println "DONE!"
 }
